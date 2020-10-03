58b43f114f83981629ab010cf5daba2a85142dbf
Raihanul Haque <raihanulhaque@rocketmail.com>
2020-10-03 18:27:45 +0600

    images of videos renders, grid fixed

--- src/components/VideoListItem.js
+++ src/components/VideoListItem.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
+import Grid from '@material-ui/core/Grid';
+import '../components/video-list-item.css';
+
 import '../components/video-list-item.css'
 
-export const VideoListItem = () => {
-    return (
-        <div>
+const VideoListItem = (props) => {
+    console.log(props.videoInfo)
+    const videoItems = props.videoInfo.map(item => 
+        <Grid item xs={12} sm={12} md={4} lg={3} xl={2} className="cardVideoListItemContainer">
             <Card className="cardVideoListItem">
-                <img src="https://i.ytimg.com/vi/Zr3PukaVXFo/sddefault.jpg" alt="" width="100%"/>
+                <img src={item.snippet.thumbnails.standard.url} alt="" width="100%"/>
                 <div className="details">
-                <h5>Video title details</h5>
-                <span>Channel name</span> <br/>
-                <span>Other info</span>
+                <h5>{item.snippet.title}</h5>
+                <span>{item.snippet.channelTitle}</span> <br/>
+                <span>Published at {item.snippet.publishedAt}</span>
                 </div>
             </Card>
+        </Grid>
+    )
+    return (
+        <div>
+        <Grid container>
+        {videoItems}
+        </Grid>
         </div>
     )
 };
